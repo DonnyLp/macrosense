af1790498fb0f3eaf8de9bea4ba2a689e538bc82
DonnyLp <dondariolapointe8@gmail.com>
2024-01-10 16:50:41 -0500

    fix __str__ method to return username on object retrival

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,15 +1,12 @@
 from django.db import models
-
-# model for user
-class User(models.Model):
+from django.contrib.auth.models import AbstractUser
+# updated user class with weight, height, age, and profilepic fields 
+class User(AbstractUser):
     user_id = models.AutoField(primary_key=True)
-    user_name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    weight = models.IntegerField()
-    height = models.IntegerField()
-    age = models.IntegerField()
+    weight = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    age = models.IntegerField(default=0)
     profile_pic = models.ImageField(upload_to='profile_pic', blank=True)
 
     def __str__(self):
-        return self.name
+        return self.username
