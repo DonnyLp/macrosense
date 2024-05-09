4f7b446be22fc6696cc26d1a4c9e2e7f6867a0db
donnylp <dondariolapointe8@gmail.com>
2024-05-08 23:36:05 -0400

    Refactored models to have consist relationships between meal, food, and log relations.

--- backend/tracking/models.py
+++ backend/tracking/models.py
@@ -4,19 +4,23 @@ from django.urls import reverse
     
 #user's macro goals
 class Goal(models.Model):
-    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    
-    # Need logic to compare these macros against daily logs
-    
-    calories = models.IntegerField(default=0) 
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    calories = models.IntegerField(default=0)
     protein = models.IntegerField(default=0)
     fat = models.IntegerField(default=0)
     carbs = models.IntegerField(default=0)
+    class Meta:
+        """
+        configures default ordering of queryset by date
+        """
+        ordering = ['date']
+        app_label = 'tracking'
+     
     
     @property 
     def __str__(self):
-        return self.user_id.username + " " + str(self.date)
-    
+        return self.user.username + " " + str(self.date)
     """
     provides a URL for the given model
     """
@@ -24,10 +28,13 @@ class Goal(models.Model):
         
         return reverse('Goal', args=[str(self.user_id)])
     
-# user's log for a given day
+"""
+Model that holds a user's log for a given day 
+Many-To-One relation: "User"
+"""
 class Log(models.Model):
-    # model fields
-    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    #total macros for the day has to be accumlated from the food entries
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField(auto_now=False, auto_now_add=True)
     total_calories = models.IntegerField(default=0)
     total_protein = models.IntegerField(default=0)
@@ -39,22 +46,19 @@ class Log(models.Model):
         configures default ordering of queryset by date
         """
         ordering = ["date"]
-        
-    
-    
-    #calculate totals for the log entry
-    def calculate_totals(self):
-        food = FoodEntry.objects.filter(log_id=self)
+           
+    # save method to calculate totals
+    def save(self, *args, **kwargs):
+        food = Meal.objects.filter(log_id=self)
         self.total_calories = sum(food.values_list('food_id__calories', flat=True))
         self.total_protein = sum(food.values_list('food_id__protein', flat=True))
         self.total_fat = sum(food.values_list('food_id__fat', flat=True))
         self.total_carbs = sum(food.values_list('food_id__carbs', flat=True))
-        self.save()
-     
+        super().save(*args, **kwargs)
     
     @property   
     def __str__(self):
-        return self.user_id.username + " " + str(self.date)
+        return self.user.username + " " + str(self.date)
     
     """
     provides a URL for the given model
@@ -63,49 +67,12 @@ class Log(models.Model):
         
         return reverse('Log', args=[str(self.user_id)])
 
-#food item relation
-class Food(models.Model):
-    food_id = models.AutoField(primary_key=True)
-    food_name = models.CharField(max_length=50)
-    calories = models.IntegerField(default=0)
-    protein = models.IntegerField(default=0)
-    fat = models.IntegerField(default=0)
-    carbs = models.IntegerField(default=0)
-    serving_size = models.IntegerField(default=0)
-    
-    
-    class Meta():
-        """
-        configures default queryset by food name
-        """
-        ordering = ['food_name']
-        
-    
-    #update macros based on serving size
-    def update_macros(self):
-        self.calories *= self.serving_size
-        self.protein *= self.serving_size
-        self.fat *= self.serving_size
-        self.carbs *= self.serving_size
-        self.save()
-        
-    
-    @property 
-    def __str__(self):
-        return self.food_name + str(self.food_id)
-    
-    """
-    provides URL for the given model
-    """
-    def get_absolute_url(self):
-    
-        return reverse('Food-Item', args=[str(self.food_id)]) 
-    
-
-#food entry for a given log
-class FoodEntry(models.Model):
-    food_id = models.ForeignKey('Food', on_delete=models.CASCADE)
-    log_id = models.ForeignKey('Log', on_delete=models.CASCADE)
+"""
+Model that holds a meal for a given log
+Many-To-One relation: "Food"
+"""
+class Meal(models.Model):
+    log = models.ForeignKey('Log', on_delete=models.CASCADE)
     time = models.TimeField(auto_now=False, auto_now_add=True)
     
     class Meta:
@@ -125,3 +92,39 @@ class FoodEntry(models.Model):
     @property
     def __str__(self):
             return self.food_id.food_name + " " + str(self.log_id.date)
+        
+# model that holds data for a food item
+class Food(models.Model):
+    food_id = models.AutoField(primary_key=True)
+    meal = models.ForeignKey('Meal',on_delete=models.CASCADE, null=True) # set null to true to defined a food relation without connection
+    food_name = models.CharField(max_length=50)
+    serving_size = models.IntegerField(default=0)    
+    calories = models.IntegerField(default=0)
+    protein = models.IntegerField(default=0)
+    fat = models.IntegerField(default=0)
+    carbs = models.IntegerField(default=0)
+    
+    def save(self, *args, **kwargs):
+        self.calories = self.calories * self.serving_size
+        self.protein = self.protein * self.serving_size
+        self.fat = self.fat * self.serving_size
+        self.carbs = self.carbs * self.serving_size
+        super().save(*args, **kwargs)
+    
+    
+    class Meta():
+        """
+        configures default queryset by food name
+        """
+        ordering = ['food_name']
+        
+    @property 
+    def __str__(self):
+        return self.food_name + str(self.food_id)
+    
+    """
+    provides URL for the given model
+    """
+    def get_absolute_url(self):
+    
+        return reverse('Food-Item', args=[str(self.food_id)]) 
