1672fce28ddfbad1e54ce7e467ad553460e3bfb1
DonnyLp <dondariolapointe8@gmail.com>
2024-05-21 03:05:04 -0400

    Added dotenv import and added load_dotenv method

--- main/macrosense/settings.py
+++ main/macrosense/settings.py
@@ -13,10 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os 
 from pathlib import Path
 from dotenv import load_dotenv
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#loading the environment variables
+
+#load the environment variables from the .env file
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
